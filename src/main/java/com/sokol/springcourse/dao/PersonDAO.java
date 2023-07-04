3ff262db47323e70fcc7a73ad302fcfcc4da10f8
sokolserg <sergsokolkin@gmail.com>
2023-07-04 18:39:44 +0300

    DAO pattern, CRUD

---
+++ src/main/java/com/sokol/springcourse/dao/PersonDAO.java
@@ -0,0 +1,29 @@
+package com.sokol.springcourse.dao;
+
+import com.sokol.springcourse.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "John"));
+        people.add(new Person(++PEOPLE_COUNT, "Jane"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(p -> p.getId() == id).findAny().orElse(null);
+    }
+}
