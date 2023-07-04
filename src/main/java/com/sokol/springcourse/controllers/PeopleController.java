50172092d4bd2b4a184976e9f3bc101240b9af80
sokolserg <sergsokolkin@gmail.com>
2023-07-04 20:33:26 +0300

    @ModelAttribute, Form Thymeleaf

--- src/main/java/com/sokol/springcourse/controllers/PeopleController.java
+++ src/main/java/com/sokol/springcourse/controllers/PeopleController.java
@@ -1,12 +1,11 @@
 package com.sokol.springcourse.controllers;
 
 import com.sokol.springcourse.dao.PersonDAO;
+import com.sokol.springcourse.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/people")
@@ -31,5 +30,16 @@ public class PeopleController {
         // We will get one person by id from the DAO and pass it to the view for display
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
